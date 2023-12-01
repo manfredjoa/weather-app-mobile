204e9f64fa8329d0b176578e281894397af00e7c
manfredjoa <joamanfred@gmail.com>
2023-12-01 15:53:07 -0500

    add bg photo and search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, StatusBar, TextInput } from "react-native";
+import { Image } from "react-native";
+import { theme } from "../theme";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="flex-1 relative">
+      <StatusBar style="light" />
+      <Image
+        blurRadius={5}
+        source={require("../assets/background.jpg")}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        <View className="h-[7%] mx-4 relative z-50">
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{ backgroundColor: theme.bgWhite(0.2) }}
+          >
+            <TextInput
+              placeholder="Search city"
+              placeholderTextColor={"lightgray"}
+              className="pl-6 h-10 flex-1 text-base text-white"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
